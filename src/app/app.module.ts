ddcb3a0d75a0c1ea8c263210dc1e40ecb8ed81c9
Kevin Lokoka <68908952+KevinL45@users.noreply.github.com>
2021-12-06 11:24:12 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestserviceService } from './service/testservice.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [TestserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
